92f5986718fd95fbcbba7b0dc881a55cfbd5b5d6
rahul-tar <divekar.rahul@gmail.com>
2016-12-19 11:13:22 -0500

    Add files via upload
    multiply nutrition values
    trim words for search

--- main.py
+++ main.py
@@ -3,54 +3,54 @@
 import sys
 
 import botinput
-import nutrition_facts.fetchData as ff
-from fuzzywuzzy import fuzz
+import fetchData
+import json
+from nltk.stem.snowball import SnowballStemmer
+
+stemmer = SnowballStemmer("english", ignore_stopwords = True)
 
 # TODO think about for spoken word input, will commas be able to deliminate foods?
 def main():
     if len(sys.argv) > 1:
         amendment_file = sys.argv[1]
         botinput.set_amendments(amendment_file) 
-    verbose = False
-    if len(sys.argv) > 2:
-        verbose = True
 
     with open("train/train.txt", "a") as logFile:
-        converse(logFile, verbose)
+        converse(logFile)
 
-def converse(logFile, verbose):
-    inputSentence = raw_input("What did you eat?\n")
+def converse(logFile):
     while True:
+        inputSentence = raw_input("what did you eat?: ")
         if inputSentence == "quit":
             break
         if len(inputSentence) <= 6:
-            inputSentence = raw_input("What else did you eat?\n")
             continue
         
-        foodList, foodTime = botinput.parse_input(inputSentence, verbose)
-        if foodTime:
-            print "================================================================"
-            print "Time:", foodTime
+        
+        foodList = botinput.parse_input(inputSentence, False) # verbose
         for food in foodList:
-            print "================================================================"
-            food.pprint()
-            nutrition = ff.getNutritionValue(food.name)
-            if nutrition:
-                print "================================================================"
-                nutrition.pprint()
-                print "================================================================"
-                confirm = raw_input("Does this information sound correct?\n")
-                if botinput.is_confirm(confirm):
-                    print "Awesome! Info has been logged into the system."
+            foodInput = food.pprint()
+            if foodInput["food"]:
+                nutritionData = fetchData.getNutritionValue(stemmer.stem(foodInput["food"]))
+               
+                if foodInput["count"]:
+                    nutritionData["Protein"] = float(nutritionData["Protein"]) * float(foodInput["count"])
+                    nutritionData["Fat"] = float(nutritionData["Fat"]) * float(foodInput["count"])
+                    nutritionData["Carbohydrate"] = float(nutritionData["Carbohydrate"]) * float(foodInput["count"])
+                    nutritionData["Total_Sugar"] = float(nutritionData["Total_Sugar"]) * float(foodInput["count"])
+                    nutritionData["Protein"] = 0
+                    print nutritionData
                 else:
-                    print "That's too bad."
+                	print nutritionData
+            continueInputSentence = raw_input("Is that data correct?: ")
+            if continueInputSentence == "yes":
+                print "Thank you"
             else:
-                print "================================================================"
-                print "Nutrition Info:\nSorry, I couldn't find that."
+                print "Sorry we got the wrong information. Help us correct it by going to this website"
+
 
         logFile.write(inputSentence + "\n")
         logFile.flush()
-        inputSentence = raw_input("What else did you eat?\n")
 
 if __name__ == "__main__":
         main()
